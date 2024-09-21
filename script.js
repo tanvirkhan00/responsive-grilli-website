f963a1325380ef2fe50c1164a2d6d8987fe9e4c1
tanvirkhan00 <kmtanvir1111@gmail.com>
2024-09-21 12:42:31 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Preload 
+// loading will be end after document is loaded
+
+const preloader = document.querySelector("[data-preaload]");
+
+window.addEventListener("load", function() {
+    preloader.classList.add("loaded");
+    document.body.classList.add("loaded")
+})
